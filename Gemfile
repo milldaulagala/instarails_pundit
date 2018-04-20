00a7178978a82a6d5dfae86884041415693f7c8b
Mill Daulagala <macos@macOSs-MacBook-Pro.local>
2018-04-20 12:09:45 +1000

    Did the db:seed

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
 
+  gem 'dotenv-rails'
+
   gem 'rspec-rails', '~> 3.7', '>= 3.7.2'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ # pending "add some examples to (or delete) #{__FILE__}"
+
+ it 'should have matching email' do
+  user = User.new
+  user.email = ENV.fetch('USER_EMAIL')
+
+  expect(user.email).to eq('abcd@gmail.com')
+
+  end
 end
